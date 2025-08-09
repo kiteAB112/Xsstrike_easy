5fdeccf30fc0c209a5cc1713aa7363d6ada0c4e7
kiteAB112 <1955269290@qq.com>
2025-08-09 13:51:45 +0800

    Day1

---
+++ core/log.py
@@ -0,0 +1,39 @@
+import logging
+from venv import logger
+# from .colors import *
+
+log_file = 'xsstrike.log'
+console_log_level = 'info'
+file_log_level = None
+
+
+def setup_logger(name='xsstrike'):
+    logger = logging.getLogger(name) # logger
+    logger.setLevel(logging.DEBUG)
+
+    if logger.handlers:
+        return logger
+
+    # formatter
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    
+    # console handler
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(getattr(logging, console_log_level.upper(), logging.INFO))
+    console_handler.setFormatter(formatter)
+
+    # addHandler
+
+    logger.addHandler(console_handler)
+
+    return logger
+
+
+logger = setup_logger()
+
+# 1. 标准日志级别（DEBUG/INFO/WARNING/ERROR/CRITICAL）
+logger.debug("调试信息：参数 x = 10")  # 最低级别，通常用于开发调试
+logger.info("程序启动成功")           # 普通运行信息
+logger.warning("注意：即将达到请求上限")  # 警告信息（不影响运行但需关注）
+logger.error("请求失败：连接超时")    # 错误信息（功能受影响）
+logger.critical("致命错误：数据库连接中断")  # 最高级别（程序可能终止）
