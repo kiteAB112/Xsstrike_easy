cf0c2be6eab6661ae42cacfb11c62f3771402447
kiteAB112 <1955269290@qq.com>
2025-08-11 23:01:38 +0800

    expand_day3

--- core/log.py
+++ core/log.py
@@ -6,6 +6,42 @@ log_file = 'xsstrike.log'
 console_log_level = 'info'
 file_log_level = None
 
+"""
+Default Logging Levels
+CRITICAL = 50
+ERROR = 40
+WARNING = 30
+INFO = 20
+DEBUG = 10
+"""
+
+VULN_LEVEL_NUM = 60
+RUN_LEVEL_NUM = 22
+GOOD_LEVEL_NUM = 25
+
+logging.addLevelName(VULN_LEVEL_NUM, 'VULN')
+logging.addLevelName(RUN_LEVEL_NUM, 'RUN')
+logging.addLevelName(GOOD_LEVEL_NUM, 'GOOD')
+
+def _vuln(self, msg, *args, **kwargs):
+    if self.isEnabledFor(VULN_LEVEL_NUM):
+        self._log(VULN_LEVEL_NUM, msg, args, **kwargs)
+
+
+def _run(self, msg, *args, **kwargs):
+    if self.isEnabledFor(RUN_LEVEL_NUM):
+        self._log(RUN_LEVEL_NUM, msg, args, **kwargs)
+
+
+def _good(self, msg, *args, **kwargs):
+    if self.isEnabledFor(GOOD_LEVEL_NUM):
+        self._log(GOOD_LEVEL_NUM, msg, args, **kwargs)
+
+logging.Logger.vuln = _vuln
+logging.Logger.run = _run
+logging.Logger.good = _good
+
+
 
 def setup_logger(name='xsstrike'):
     logger = logging.getLogger(name) # logger
